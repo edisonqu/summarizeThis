812fd108de1dee72f56ada3b6eb1fc970fe87e03
edison <edisonquu@gmail.com>
2022-11-22 19:41:49 -0500

    fixed deferring and added comments for the bot.

--- main.py
+++ main.py
@@ -1,41 +1,34 @@
-
-# This example requires the 'message_content' intent.
-
-import discord
 import dotenv
 import os
-dotenv.load_dotenv()
 import discord
 from discord import Option
 from summarize import query
 
+# Initialization for the DOTENV file
+dotenv.load_dotenv()
 
+# Initialization for the Discord Bot
 bot = discord.Bot()
 
+#making sure that the event is ready
 @bot.event
 async def on_ready():
     print(f"We have logged in as {bot.user}")
 
-@bot.slash_command(guild_ids=["984609554836881470"], description= "Summarize this Text!")
-async def summarize(ctx,prompt:Option(str, description="enter prompt",name= "prompt", required=True), user:Option(discord.Member)):
-    # await ctx.channel.trigger_typing()
+# Slash command for this specific guild
+@bot.slash_command(description= "Summarize this Text!")
+async def summarize(ctx,prompt:Option(str, description="enter prompt",name= "prompt", required=True)):
+    await ctx.defer()
     answer = query({
     "inputs": prompt
-})
-    print(prompt)
-    print(answer[0]['summary_text'])
+    })
     answers = answer[0]['summary_text']
-    try:
-        await ctx.respond(f"Summarized Text: {answers}")
-    except:
-        user = ctx.author.id
-        await ctx.send(f"<@{user}> Summarized Text: {answers}")
+    await ctx.respond("Summarized Text: "+answers)
 
-@bot.slash_command(guild_ids=["984609554836881470"], description= "Is that it?", name="food")
+# Little Easter Egg / Joke about what Elliot ate for lunch today
+@bot.slash_command(guild_ids=["984609554836881470"], description= "Did you only eat chips?", name="elliot")
 async def wasthatit(ctx,user:Option(discord.Member, "enter prompt",name= "user")):
-    if user == None:
-        user = ctx.author.id
-    await ctx.respond(f"Was that it? <@{user}>")
+    await ctx.respond(f"What did you eat this lunch? <@{user.id}>")
 
-
+# Run the bot
 bot.run(os.getenv("DB_TOKEN"))
--- summarize.py
+++ summarize.py
@@ -5,16 +5,9 @@ dotenv.load_dotenv()
 
 API_KEY_HF = os.getenv("API_KEY_HF")
 
-
 API_URL = "https://api-inference.huggingface.co/models/facebook/bart-large-cnn"
 headers = {"Authorization": f"Bearer {API_KEY_HF}"}
 
 def query(payload):
     response = requests.post(API_URL, headers=headers, json=payload)
     return response.json()
-
-# output = query({
-#     "inputs": input
-# })
-#
-# print(output)
